311a49966904aeca3300d282527521884f0be623
Карасев Илья <devnull@localhost>
2014-11-06 21:49:19 +0400

    linear approximation

--- Gprs/Gprs.c
+++ Gprs/Gprs.c
@@ -12,6 +12,7 @@
 
 #define WIND_MEASURE_S 120
 #define SPEEDS_BLOCK_LENGTH 15
+#define FACTORS 3
 #define WAIT_CONNECTION 10000
 
 #include <stdio.h>
@@ -26,10 +27,24 @@ FILE uart_stream = FDEV_SETUP_STREAM(Uart_putc, Uart_getc, _FDEV_SETUP_RW);
 
 unsigned int speeds[SPEEDS_BLOCK_LENGTH];
 
+float K[FACTORS] = { -0.00866, -0.00583, -0.003 };
+int x0[FACTORS] = { 1200, 2400, 10000 };
+int B[FACTORS] = { 33, 26, 4 };
+	
+unsigned int Speed(unsigned int x)
+{
+	long int xTemp = x;
+	for (int i = 0; i < FACTORS; i++)
+	{
+		if (xTemp < x0[i]) return (unsigned int) (K[i] * (xTemp - x0[i]) + B[i]);
+	}	
+	return 0;	
+}	
+
 int main(void)
 {	
-	long int Ticks;
-	long unsigned int StrobeSum;
+	int Ticks;
+	unsigned int SpeedSum;
 		
 	stdout = stdin = &uart_stream;	
 	Uart_init(MYUBRR);		
@@ -47,16 +62,16 @@ int main(void)
 	    for (int i = 0; i < SPEEDS_BLOCK_LENGTH; i++)
 	    {
 		    Ticks = 0;
-		    StrobeSum = 0;
+		    SpeedSum = 0;
 		    while (Ticks < WIND_MEASURE_S)
 		    {
-			    _delay_ms(1000);
-			    StrobeSum += StrobeTime;
+			    _delay_ms(995);
+			    SpeedSum += Speed(StrobeTime);
 			    Ticks++;
 				wdt_reset();
 		    }
 		    
-		    speeds[i] = (unsigned int)(StrobeSum / WIND_MEASURE_S);	
+		    speeds[i] = (unsigned int)(SpeedSum / WIND_MEASURE_S);	
 			fprintf(stdout, "%u ", speeds[i]);
 	    }
 		wdt_disable();
